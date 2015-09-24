048889c843c7fadad157f10df00a2ca04ea7c496
Koen Weyn <koenweyn@blibo.be>
2015-09-24 14:59:04 +0200

    Alias chromedev

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,7 +51,7 @@ funcsave idiff
 
 #start chrome without extensions and a clean profile
 function chromedev
-  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-extensions --user-data-dir=/tmp 2>/dev/null &
+  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --profile-directory="Profile 1" $argv 2>/dev/null &
 end
 funcsave chromedev
 
