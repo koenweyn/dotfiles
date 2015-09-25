93b52aebb570056ee2215c8d8ae57608863329cf
Koen Weyn <koenweyn@blibo.be>
2015-09-25 14:01:54 +0200

    Atom editor stylesheet

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
 # make links
+ln -sf ~/dotfiles/.atom/* ~/.atom/
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
-
--sf ~/dotfiles/IntelliJ/liveTemplates/* ~/Library/Preferences/IntelliJIdea14/templates/
+ln -sf ~/dotfiles/IntelliJ/liveTemplates/* ~/Library/Preferences/IntelliJIdea14/templates/
 
 echo "Successfully updated dotfiles!"
