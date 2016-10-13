e9f01936b28a6ad883d807aa9f056b97a84a6e84
Koen Weyn <koenweyn@blibo.be>
2016-10-13 17:28:09 +0200

    Add git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,9 @@ set -e # Any subsequent(*) commands which fail will cause the shell script to ex
 ln -sf ~/dotfiles/.atom/* ~/.atom/
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
-ln -sf ~/dotfiles/.config/fish/functions ~/.config/fish/functions
+ln -sf ~/dotfiles/.config/fish/functions ~/.config/fish/
 ln -sf ~/dotfiles/IntelliJ/liveTemplates/* ~/Library/Preferences/IntelliJIdea14/templates/
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
 echo "Successfully updated dotfiles!"
