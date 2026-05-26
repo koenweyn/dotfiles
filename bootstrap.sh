24cb936dd4498937ca4edac1939afafa79f3b466
Koen Weyn <koenweyn@blibo.be>
2026-05-26 15:40:53 +0200

    Delete atom and IntelliJ live templates

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,11 @@
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
 # make links
-mkdir -p ~/.atom/
 mkdir -p ~/.config/fish
-mkdir -p ~/Library/Preferences/IntelliJIdea14/templates/
 
-ln -sf ~/dotfiles/.atom/* ~/.atom/
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/.config/fish/functions ~/.config/fish/
-ln -sf ~/dotfiles/IntelliJ/liveTemplates/* ~/Library/Preferences/IntelliJIdea14/templates/
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
